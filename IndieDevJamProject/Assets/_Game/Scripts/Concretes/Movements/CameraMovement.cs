a08e9efa1d33fea57f943c3ea5db5e4b26c59d8f
MertBalkan <mert__balkan@hotmail.com>
2024-02-27 01:56:43 +0300

    Update CameraMovement.cs

--- IndieDevJamProject/Assets/_Game/Scripts/Concretes/Movements/CameraMovement.cs
+++ IndieDevJamProject/Assets/_Game/Scripts/Concretes/Movements/CameraMovement.cs
@@ -14,7 +14,11 @@ namespace SnaileyGame.Movements
         public float difficultyDecreaseRate = 0.2f;
         public float minimumDifficulty = 0.6f;
         public float increaseRate = 2;
-      
+
+
+        private bool v1 = false;
+        private bool v2 = false;
+        private bool v3 = false;
 //200
 //480
 //875
@@ -33,19 +37,16 @@ namespace SnaileyGame.Movements
             if (transform.position.y >= 195 && transform.position.y <= 480)
             {
                 _hookController.IncreaseHookValue(4f);
-                cameraMoveSpeed += 1;
             }
             
             if (transform.position.y > 480 && transform.position.y < 875f)
             {
                 _hookController.IncreaseHookValue(5f);
-                cameraMoveSpeed += 1;
             }
             
             if (transform.position.y >= 875f)
             {
                 _hookController.IncreaseHookValue(6);
-                cameraMoveSpeed += 1;
             }
         }
 
